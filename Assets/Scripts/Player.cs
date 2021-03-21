278480f17e587e299a8b21751841ce79a8328b8e
lwildmann <wildmannlukas03@gmail.com>
2021-03-21 17:33:15 +0100

    create sprite and scripts folder, finish script

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Player : MonoBehaviour
+{
+    public Vector3 mPosition;
+
+    public Vector3 mCamPosition;
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            mPosition = Input.mousePosition;
+            mCamPosition = Camera.main.ScreenToWorldPoint(mPosition);
+            this.transform.position = mCamPosition;
+        }
+    }
+}
