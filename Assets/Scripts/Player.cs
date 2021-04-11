3965e27327f75b7f88a3cc480f314f78e1196ab4
lwildmann <wildmannlukas03@gmail.com>
2021-04-11 10:42:44 +0200

    point to click player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,13 +8,27 @@ public class Player : MonoBehaviour
 
     public Vector3 mCamPosition;
 
+    public Vector3 playerPos;
+
+    public float speed = 0.1f;
+
+    void Start()
+    {
+        playerPos = this.transform.position;
+    }
     void Update()
     {
-        if (Input.GetMouseButton(0))
+
+        if (Input.GetMouseButtonDown(0))
         {
             mPosition = Input.mousePosition;
             mCamPosition = Camera.main.ScreenToWorldPoint(mPosition);
-            this.transform.position = mCamPosition;
+            playerPos = new Vector3(mCamPosition.x, mCamPosition.y, this.transform.position.z);
+
         }
+        
+            
+            this.transform.position = Vector3.MoveTowards(this.transform.position, playerPos, speed);
+        
     }
 }
