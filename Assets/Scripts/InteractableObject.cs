cced492e72bdb48d36c43600b8eecff58da31be5
lwildmann <wildmannlukas03@gmail.com>
2021-04-18 14:03:42 +0200

    Finish Interactable Object Script

---
+++ Assets/Scripts/InteractableObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InteractableObject : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        this.gameObject.active = false;
+        GameObject.Find("ObjectName").GetComponent<Text>().text = this.gameObject.name;
+        Debug.Log("Es funktioniert!");
+    }
+
+
+
+}
